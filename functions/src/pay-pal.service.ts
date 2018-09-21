0d9a45b8366b3cb4d438faa9098a26ffe9468727
Tony Bellomo <CoderTonyB@gmail.com>
2018-09-20 22:27:50 -0400

    parsing data with automatic date range from paypal

--- functions/src/pay-pal.service.ts
+++ functions/src/pay-pal.service.ts
@@ -4,67 +4,63 @@ import { PaypalTransaction } from './models/PaypalTransaction';
 import * as WebRequest from 'web-request';
 
 exports.updatePaypal = functions.https.onRequest(async (req, res) => {
-  let output: string;
-  output = 'test output';
+  let output: string = '';
 
   const token = await getPaypalToken();
 
   console.log('New Token:', token);
 
-  const data = await getPaypalData(token);
+  const data: PaypalTransaction = await getPaypalData(token);
 
-  //   data.transaction_details.forEach(transaction => {
-  //     output += '\n---------------------------------------';
-  //     output += '\nId:' + transaction.transaction_info.transaction_id;
-  //     output +=
-  //       '\ndate:' + transaction.transaction_info.transaction_initiation_date;
-  //     output +=
-  //       '\nAmount:' + transaction.transaction_info.transaction_amount.value;
-  //     output += '\nemail:' + transaction.payer_info.email_address;
-  //     output += '\nname:' + JSON.stringify(transaction.payer_info.payer_name);
-  //   });
+  data.transaction_details.forEach(transaction => {
+    output += '\n---------------------------------------';
+    output += '\nId:' + transaction.transaction_info.transaction_id;
+    output +=
+      '\ndate:' + transaction.transaction_info.transaction_initiation_date;
+    output +=
+      '\nAmount:' + transaction.transaction_info.transaction_amount.value;
+    output += '\nemail:' + transaction.payer_info.email_address;
+    output += '\nname:' + JSON.stringify(transaction.payer_info.payer_name);
+  });
 
-  res.send(JSON.stringify(data));
+  res.send(output);
 });
 
-// async function getPaypalData(token: string) {
-//   const btoa = require('btoa');
-
-//   const result: WebRequest.Response<string> = await WebRequest.get(
-//     'https://api.sandbox.paypal.com/v1/reporting/transactions?start_date=2018-06-01T00:00:00-0700&end_date=2018-07-01T00:00:00-0700&fields=payer_info&page_size=500',
-//     {
-//       headers: {
-//         Authorization: 'Bearer ' + token,
-//         'Content-Type': 'application/json'
-//       },
-//       form: {},
-//       json: true
-//     }
-//   )
-//     .then(response => response.message)
-//     .catch(error => error);
-
-//   return result;
-// }
-
 async function getPaypalData(token: string) {
-  const result: WebRequest.Response<string> = await WebRequest.get(
-    'https://api.sandbox.paypal.com/v1/reporting/transactions?start_date=2018-06-01T00:00:00-0500&end_date=2018-07-01T00:00:00-0500&fields=transaction_info,payer_info&page_size=500',
+  const endDate: Date = new Date(Date.now());
+  const startDate: Date = new Date();
+  startDate.setDate(endDate.getDate() - 31);
+
+  const payPalStartDate =
+    startDate.getFullYear() +
+    '-' +
+    (startDate.getMonth() + 1) +
+    '-' +
+    startDate.getDate();
+
+  const payPalEndDate =
+    endDate.getFullYear() +
+    '-' +
+    (endDate.getMonth() + 1) +
+    '-' +
+    endDate.getDate();
+
+  console.log(payPalStartDate, '-', payPalEndDate);
+
+  return await WebRequest.get(
+    `https://api.sandbox.paypal.com/v1/reporting/transactions?start_date=${payPalStartDate}T00:00:00-0500&end_date=${payPalEndDate}T00:00:00-0500&fields=all&page_size=500`,
     {
       headers: {
         Authorization: 'Bearer  ' + token,
         'Content-Type': 'application/json'
       },
-      // form: {
-      //   grant_type: 'client_credentials'
-      // },
       json: true
     }
   )
-    .then(res => res.message || res.statusMessage)
+    .then(res => {
+      return res.message['body'];
+    })
     .catch(error => error);
-
-  return result;
 }
 
 async function getPaypalToken() {
