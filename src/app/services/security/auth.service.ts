68570c805a7cedc5a78959d0c050334a10874820
Tony Bellomo <CoderTonyB@gmail.com>
2021-03-06 17:39:22 -0500

    using localstorage to set token on reload of tab

--- src/app/services/security/auth.service.ts
+++ src/app/services/security/auth.service.ts
@@ -1,18 +1,29 @@
-import { Injectable } from "@angular/core";
-import { Observable, BehaviorSubject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
-import { Router } from "@angular/router";
-import { TimerObservable } from "rxjs/observable/TimerObservable";
+import { Router } from '@angular/router';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
 
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { environment } from "../../../environments/environment";
-import { take, tap } from "rxjs/operators";
-import { Member } from "../../entities/member";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { take, tap } from 'rxjs/operators';
+import { Member } from '../../entities/member';
 
 @Injectable()
 export class AuthService {
   isAuthenticated: boolean;
-  public authToken = "";
+  private _authToken = '';
+  public get authToken() {
+    if (this._authToken == '') {
+      this._authToken = localStorage.getItem('token');
+    }
+    return this._authToken;
+  }
+
+  public set authToken(v: string) {
+    this._authToken = v;
+  }
+
   // displayName$: BehaviorSubject<string> = new BehaviorSubject<string>('');
   isAuthenticated$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
     false
@@ -25,11 +36,12 @@ export class AuthService {
   public login(email: string, password: string): Observable<any> {
     const postData = { email, password };
     return this.http
-      .post<any>(environment.TonicAPIURL + "login", postData)
+      .post<any>(environment.TonicAPIURL + 'login', postData)
       .pipe(
         tap((result) => {
           this.authToken = result.token;
           this.member = result.member;
+          localStorage.setItem('token', result.token);
         })
       );
   }
