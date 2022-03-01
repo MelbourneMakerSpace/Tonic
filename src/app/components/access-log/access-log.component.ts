cbc2183464904a3e64e5f848a4fa6aa8329bd9a0
Brian Beale <brianbeale@protonmail.com>
2022-03-01 18:53:56 -0500

    Join names, add access icons

--- src/app/components/access-log/access-log.component.ts
+++ src/app/components/access-log/access-log.component.ts
@@ -15,12 +15,14 @@ export class AccessLogComponent implements OnInit {
 
   @ViewChild(MatSort) sort: MatSort;
   displayedColumns = [
-    'firstname',
-    'lastname',
+    // 'firstname',
+    // 'lastname',
+    'name',
     'Status',
     'accessGranted',
     'message',
-    'timestamp',
+    'time',
+    'email',
   ];
   constructor(
     private accessLogService: AccessLogService,
@@ -30,8 +32,8 @@ export class AccessLogComponent implements OnInit {
   ngOnInit(): void {
     this.accessLogService.getAccessLog().subscribe((accessList) => {
       this.accessLogReport = accessList;
-      console.log(`accessLogReport: ${this.accessLogReport}`);
-      console.log(`accessLogReport: ${this.accessLogReport[0].message}`);
+      // console.log(`accessLogReport: ${this.accessLogReport}`);
+      // console.log(`accessLogReport: ${this.accessLogReport[0].message}`);
       this.dataSource = new MatTableDataSource(this.accessLogReport);
     });
   }
