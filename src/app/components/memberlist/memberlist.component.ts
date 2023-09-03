e345b371f6ea1fdcbbc772b22c4c1c77e702731d
Tony Bellomo <tony.bellomo@erm.com>
2023-09-03 11:05:55 -0400

    filters saving on page change

--- src/app/components/memberlist/memberlist.component.ts
+++ src/app/components/memberlist/memberlist.component.ts
@@ -34,47 +34,40 @@ export class MemberlistComponent implements OnInit, AfterViewInit {
   public memberList = [];
   public activeMemberCount: any = 'Counting...';
   dataSource;
-  currentListFilter = null;
+  currentListFilter = '';
+  showInactive = false;
 
   @ViewChild(MatSort) sort: MatSort;
   displayedColumns = ['firstname', 'lastname', 'email', 'Status'];
-  public filter$ = new BehaviorSubject(null);
+  public filter$ = new BehaviorSubject('x');
   constructor(private memberService: MemberService, private router: Router) {}
 
   ngAfterViewInit() {}
 
   toggleShowInactive(toggle) {
-    this.showInactive = toggle.checked;
+    this.showInactive = toggle;
+    sessionStorage.setItem('showInactive', toggle ? '1' : '0');
     this.filter$.next(this.currentListFilter);
-  }
-
-  public get showInactive(): boolean {
-    console.log(`showInactive: ${sessionStorage.getItem('showInactive')}`);
-    return sessionStorage.getItem('showInactive') == '1' ? true : false;
-  }
-
-  public set showInactive(flag: boolean) {
-    sessionStorage.setItem('showInactive', flag ? '1' : '0');
   }
 
   ngOnInit() {
     this.filter$.pipe(debounceTime(400)).subscribe((filterstring) => {
       this.currentListFilter = filterstring;
 
-      if (filterstring?.length > 0) {
-        // Need to set memberList = members so that we re-search the whole
-        // list, not just the current filtered list
-        this.memberService.getMemberList().subscribe((members) => {
-          this.memberList = members;
-        });
+      this.memberService.getMemberList().subscribe((members) => {
+        this.memberList = members;
 
-        this.memberList = this.memberList.filter(
-          (member) =>
-            member.firstName
-              .toLowerCase()
-              .startsWith(filterstring.toLowerCase()) ||
-            member.lastName.toLowerCase().startsWith(filterstring.toLowerCase())
-        );
+        if (filterstring?.length > 0) {
+          this.memberList = this.memberList.filter(
+            (member) =>
+              member.firstName
+                .toLowerCase()
+                .startsWith(filterstring.toLowerCase()) ||
+              member.lastName
+                .toLowerCase()
+                .startsWith(filterstring.toLowerCase())
+          );
+        }
 
         if (!this.showInactive) {
           this.memberList = this.memberList.filter(
@@ -83,33 +76,27 @@ export class MemberlistComponent implements OnInit, AfterViewInit {
         }
 
         this.dataSource = new MatTableDataSource(this.memberList);
-      } else {
-        this.memberService.getMemberList().subscribe((members) => {
-          this.memberList = members;
+        this.dataSource.sort = this.sort;
 
-          if (this.showInactive) {
-            this.dataSource = new MatTableDataSource(this.memberList);
-          } else {
-            this.dataSource = members.filter(
-              (member) => member.status === 'Active'
-            );
-          }
-          this.dataSource.sort = this.sort;
-          this.activeMemberCount = this.memberList.filter(
-            (member) => member.status === 'Active'
-          ).length;
-        });
-      }
+        this.activeMemberCount = this.memberList.filter(
+          (member) => member.status === 'Active'
+        ).length;
+      });
     });
+
+    this.showInactive = sessionStorage.getItem('showInactive') == '1';
+    console.log('showInactive', this.showInactive);
+    this.applyFilter(sessionStorage.getItem('filterString') ?? '');
   }
 
   applyFilter(filterstring: string) {
+    sessionStorage.setItem('filterString', filterstring);
     this.filter$.next(filterstring);
   }
 
-  clearFilter(filterInput) {
+  clearFilter() {
     this.filter$.next('');
-    filterInput.value = '';
+    this.currentListFilter = '';
   }
 
   addMember(event) {
